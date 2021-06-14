e306947447b4b6676a20504bb1c248f3939f5314
Jakob <jakobsvensson92@gmail.com>
2021-06-14 15:24:51 +0200

    Fix splash screen

--- js/views/splash.js
+++ js/views/splash.js
@@ -4,18 +4,24 @@ import m from "mithril";
 let splash = {
     view: function() {
         return [
-            m("div.diagonal", [
-                m("div.content", [
-                    m("div.splashlogo"),
-                    m("div.splashtitle", "Staring at a blank page?"),
-                    m("div.splashtext", `WIVI helps you break creative block
-                    by using randomized, creative prompts. Instead of endless 
-                    feed scrolling, you will get two images, a title and a 'Strategy'. 
-                    Use this to make a quick sketch, take a photo, write a poem or 
-                    maybe a LEGO sculpture.`),
-                    m("a.finishedBtn", { href: "#!/frontpage"}, "LET'S GO!")
-                ])
+            m("div.content", [
+                m("div.splashlogo"),
+                m("div.splashtitle", "Staring at a blank page?"),
+                m("div.bgtext", `WIVI helps you break creative block
+                            by using randomized, creative prompts.`),
+                m("div.bgtext", `Instead of endless 
+                            feed scrolling, you will get two images, a title and a 'Strategy'. 
+                            Use this to make a quick sketch, take a photo, write a poem or 
+                            maybe a LEGO sculpture.`),
+                m("a.diagBtn", { href: "#!/frontpage" }, "START WIVI"),
+                m("div.diagBG", [
+    
+                    m("div.diagonal", [
+    
+                    ])
+                ]),
             ])
+
 
         ];
     }
