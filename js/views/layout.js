bfb07c3bfb37c352598346a7a5bd2b6524ed3b5c
Jakob <jakobsvensson92@gmail.com>
2021-06-15 01:13:25 +0200

    Add logo link

--- js/views/layout.js
+++ js/views/layout.js
@@ -6,10 +6,10 @@ let layout = {
     view: function (vnode) {
         return [
             m("nav.top-nav", [
-                m("a.nav-home fas fa-house-user", { href: "#!/" }),
+                m("a.nav-home fas fa-house-user", { href: "#!/frontpage" }),
             ]),
             m("div.topbar"),
-            m("a.logo", { href: "#!/" }),
+            m("a.logo", { href: "#!/frontpage" }),
             m("main.container", vnode.children)
         ];
     }
